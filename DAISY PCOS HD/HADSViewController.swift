e588df4a2af6482544d6ca4cfd326e4aeeac5969
xiandong <dongxian.bruce@gmail.com>
2019-03-18 21:38:31 +1100

    Can check the HADS selected answers

--- DAISY PCOS HD/HADSViewController.swift
+++ DAISY PCOS HD/HADSViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class HADSViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let questionArray = HADSQuestionBank().list
-    var pickedAnswer : Int = 0
-    var questionNumber : Int = 0
+    var selectedAnswer = [Int]()
     var depressionScore : Int = 0
     var anxietyScore : Int = 0
     
@@ -22,14 +21,18 @@ class HADSViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         questionTable.delegate = self
         questionTable.dataSource = self
         
         questionTable.register(UINib(nibName: "HADSTableViewCell", bundle: nil), forCellReuseIdentifier: "HADSTableViewCell")
         
         // configureTableView()
-        
+        if (questionTable.contentSize.height < questionTable.frame.size.height) {
+            questionTable.isScrollEnabled = false
+        } else {
+            questionTable.isScrollEnabled = true
+        }
         questionTable.separatorStyle = .none
         
     }
@@ -40,7 +43,7 @@ class HADSViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-         let cell = tableView.dequeueReusableCell(withIdentifier: "HADSTableViewCell", for: indexPath) as! HADSTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HADSTableViewCell", for: indexPath) as! HADSTableViewCell
         
         cell.questionLabel.text = questionArray[indexPath.row].questionText
         cell.optionA.setTitle(questionArray[indexPath.row].answer[0], for: .normal)
@@ -56,13 +59,35 @@ class HADSViewController: UIViewController, UITableViewDelegate, UITableViewData
         questionTable.rowHeight = UITableView.automaticDimension
         questionTable.estimatedRowHeight = 280.0
     }
-
-    @IBAction func question1Pressed(_ sender: Any) {
+    
+    @IBAction func submitPressed(_ sender: Any) {
         
-        print((sender as! UIButton).tag)
+        // print((sender as! UIButton).tag)
+        
+        // Check which buttons are selected for each question
+        for n in 0...questionArray.count-1 {
+            
+            let cell = questionTable.cellForRow(at: IndexPath(row: n, section: 0)) as! HADSTableViewCell
+            
+            if (cell.optionA.isSelected) { selectedAnswer.append(0) }
+            else if (cell.optionB.isSelected) { selectedAnswer.append(1) }
+            else if (cell.optionC.isSelected) { selectedAnswer.append(2) }
+            else if (cell.optionD.isSelected) { selectedAnswer.append(3) }
+            else {
+                // WARNING: MUST FINISH ALL QUESTIONS BEFORE SUBMIT
+            }
+            
+        }
+        
+        // Print the answer in the console
+        for n in 0...questionArray.count-1 {
+            print("Question\(n+1): \(selectedAnswer[n])")
+        }
+        
+        // Return to PartB
+        // performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
         
     }
-    
     
     
 }
